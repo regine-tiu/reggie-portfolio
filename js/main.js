33289dabf75ca92de5ab90db75ce5310808be43f
Regine Tiu <regine.d.tiu@gmail.com>
2024-03-06 18:04:23 -0500

    added jquery filter function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
-$(document).ready(() => {
-
+$(document).ready(function() {
+    $('.filter-button').on('click', function() {
+        var filter = $(this).data('filter');
+        $('.project-item').hide();
+        $('.' + filter).show();
+    });
 });
