ca5cee98cb28c1ea987e90c04ae8dc4100b98a9e
Regine Tiu <regine.d.tiu@gmail.com>
2024-03-08 11:31:19 -0500

    added project filter jquery

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
     $('.filter-button').on('click', function() {
-        var filter = $(this).data('filter');
-        $('.project-item').hide();
-        $('.' + filter).show();
+        $(this).toggleClass('clicked'); // Toggle 'clicked' class on the clicked button
+        
+        var filters = []; // Array to store selected filters
+        $('.filter-button.clicked').each(function() {
+            filters.push($(this).data('filter')); // Add selected filters to the array
+        });
+        
+        if (filters.length > 0) {
+            $('.project-item').hide();
+            filters.forEach(function(filter) {
+                $('.' + filter).show(); // Show project items with selected filters
+            });
+        } else {
+            $('.project-item').show(); // If no filters selected, show all project items
+        }
     });
 });
